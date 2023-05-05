bb27d9c53969ffe358e2f93458e6b0e9cd6bdcb1
苏小小 <xudong.tian@hand-china.com>
2023-05-05 11:13:05 +0800

    init

--- src/components/SvgIcon/index.jsx
+++ src/components/SvgIcon/index.jsx
@@ -1,19 +1,27 @@
 import PropTypes from 'prop-types';
 
 const SvgIcon = (props) => {
-  const {name, className = "", overHidden = true} = props
+  const {
+    name, className = "",
+    title = "",
+    overHidden = true,
+    onClick, onDoubleClick
+  } = props
 
   return (
-    <svg className={"icon " + className} aria-hidden={overHidden}>
+    <svg className={"icon " + className} title={title} aria-hidden={overHidden} onClick={onClick} onDoubleClick={onDoubleClick}>
       <use xlinkHref={`#icon-${name}`}></use>
     </svg>
   )
 }
 
 SvgIcon.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string,
   overHidden: PropTypes.bool,
-  className: PropTypes.string
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  onDoubleClick: PropTypes.func
 }
 
 export default SvgIcon
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,28 +3,78 @@ import {getName, getVersion} from "@tauri-apps/api/app";
 import PropTypes from "prop-types";
 import SvgIcon from "@/components/SvgIcon/index.jsx";
 
+import {getCurrent} from '@tauri-apps/api/window'
+
+const currtWin = getCurrent()
+
+const _WinBtn = (props) => {
+  const {onClose, onMin, onMax, onReset} = props
+  const [isMaximized, setMaximizable] = useState(false)
+
+  const init = async () => {
+    setMaximizable(await currtWin.isMaximized())
+  }
+  useEffect(() => {
+    init().then()
+  }, [])
+
+
+  const onWinMax = async () => {
+    isMaximized ? (await currtWin.unmaximize()) : (await currtWin.maximize())
+    setMaximizable(await currtWin.isMaximized())
+    isMaximized ? onMax() : onReset()
+  }
+
+  const onWinMin = async () => {
+    await currtWin.minimize()
+    onMin()
+  }
+
+  const onCloseWin = async () => {
+    await currtWin.close()
+    onClose()
+  }
+
+
+  return (
+    <div className="taui__winbtn h-full w-fit p-2 float-right right-0 flex items-center justify-end">
+      <SvgIcon name="Generalmoon-" title="主题" className={"mx-1"}/>
+      <SvgIcon name="Controlhelp-" className={"mx-1"}/>
+      <SvgIcon name="Controlchevrondown-" className={"mx-1"} onClick={onWinMin}/>
+      <SvgIcon name={`Arrowmove-${isMaximized ? '' : '1'}`} className={"mx-1"} onClick={onWinMax}/>
+      <SvgIcon name="Controlclose-" className={"mx-1"} onClick={onCloseWin}/>
+    </div>
+  )
+}
+_WinBtn.protoTypes = {
+  onCLose: PropTypes.func,
+  onMax: PropTypes.func,
+  onMin: PropTypes.func,
+  onReset: PropTypes.func,
+}
+
+export const WinBtn = _WinBtn
+
 const Header = (props) => {
   const {className: className = ""} = props
-  const [minAble] = useState(false)
   const [tauriVersion, setTauriVersion] = useState("")
+
+  const init = async () => {
+    // setFullscreen(await currtWin.isFullscreen())
+
+    // 设置头信息
+    setTauriVersion(`${await getName()}:v${await getVersion()}`)
+  }
+
   useEffect(() => {
-    const getTitle = async () => {
-      const appName = await getName()
-      const appVersion = await getVersion();
-      setTauriVersion(`${appName}:v${appVersion}`)
-    }
-    getTitle()
+    init().then()
   }, [])
 
   return (
-    <div className={"" + className.toString() + " w-full bg-amber-50 "} data-tauri-drag-region>
-      <span className="font-bold font-serif">{tauriVersion}</span>
-      <div className="taui__winbtn w-fit float-right right-0">
-        <SvgIcon name="Arrowmove-1"/>
-        <SvgIcon name="Arrowmove-1"/>
-        <SvgIcon name="Arrowmove-1"/>
-        <SvgIcon name="Arrowmove-1"/>
-      </div>
+    <div className={"" + className.toString() + " w-full bg-amber-50 flex items-center justify-between"}
+         data-tauri-drag-region>
+      <span className="font-bold font-serif p-2">{tauriVersion}</span>
+      <WinBtn/>
     </div>
   )
 }
